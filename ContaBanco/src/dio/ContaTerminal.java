ac6761e48fe11033b92ebb51b261539cfa6e009f
lazaro8728 <134394657+lazaro8728@users.noreply.github.com>
2023-10-08 10:39:47 -0300

    Update ContaTerminal.java

--- ContaBanco/src/dio/ContaTerminal.java
+++ ContaBanco/src/dio/ContaTerminal.java
@@ -1,10 +1,29 @@
 package dio;
 
+import java.util.Scanner;
+
 public class ContaTerminal {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
 
+		System.out.println("Por favor, digite o numero da Agencia:");
+		String agencia = scanner.nextLine();
+
+		System.out.println("Por favor, digite o numero da Conta:");
+		int numero = scanner.nextInt();
+
+	
+		scanner.nextLine();
+
+		System.out.println("Por favor, digite o Nome do Cliente:");
+		String nomeCliente = scanner.nextLine();
+
+		System.out.println("Por favor, digite o Saldo:");
+		double saldo = scanner.nextDouble();
+
+		System.out.println("Ola " + nomeCliente + ", obrigado por criar uma conta em nosso banco,");
+		System.out.println("sua agencia e " + agencia + ", conta " + numero + " e seu saldo " + saldo + " ja esta disponivel para saque.");
+
+		scanner.close();
 	}
-
 }
